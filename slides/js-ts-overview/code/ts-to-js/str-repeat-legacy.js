86618d9bdd27d3f30ed027cb17e568eec8b98cec
zerksis umrigar <umrigar@binghamton.edu>
2025-08-19 11:06:09 -0400

    pre-class update

--- slides/js-ts-overview/code/ts-to-js/str-repeat-legacy.js
+++ slides/js-ts-overview/code/ts-to-js/str-repeat-legacy.js
@@ -2,6 +2,6 @@ function strRepeat(str, repeat) {
     return Array.from({ length: repeat })
       .map(function (_, i) {
              return str.repeat(i + 1); 
-      });
+    });
 }
 globalThis.console.log(strRepeat('a', 4));
