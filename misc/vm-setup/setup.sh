06436000ec563fefb97093fa0d05926b58dd047e
zerksis umrigar <umrigar@binghamton.edu>
2025-08-27 13:13:47 -0400

    added vm setup directions

--- misc/vm-setup/setup.sh
+++ misc/vm-setup/setup.sh
@@ -1,28 +1,14 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
 
 mkdir -p ~/Downloads; cd ~/Downloads
 
 sudo apt-get update
 
-#https://linuxconfig.org/ubuntu-22-04-system-backup-and-restore>
-sudo apt-get install -y timeshift
-TIMESHIFT_DEV=/dev/mapper/ubuntu--vg-ubuntu--lv
-sudo timeshift --create --snapshot-device $TIMESHIFT_DEV
-sudo timeshift --list --snapshot-device $TIMESHIFT_DEV
-
-sudo apt-get install -y curl
-
-#<https://wiki.lxde.org/en/Installation>
-#installing lubuntu-core did not work
-#following command worked but snap firefox hung.
-sudo apt-get -y install lxde
 sudo apt-get -y install xfce4
 sudo apt-get -y install xfce4-goodies
-sudo apt-get install -y gnome-system-tools
+sudo apt-get -y install -y gnome-system-tools
 
 #x2go remote desktop
-#sudo apt-add-repository ppa:x2go/stable
-#sudo apt-get update
 sudo apt-get install -y x2goserver x2goclient x2goserver-xsession
 
 #set up for automatic unattended upgrades; important for security
@@ -31,38 +17,23 @@ sudo apt-get install -y unattended-upgrades
 #popular revision control system
 sudo apt-get install -y git 
 
-#install packages which may be necessary in building native js modules
+#basic devel packages
 sudo apt-get install -y software-properties-common build-essential libssl-dev
-sudo apt-get install -y apt-transport-https
-
-#install nodejs
-curl -fsSL https://deb.nodesource.com/setup_23.x | sudo -E bash - &&\
-sudo apt-get install -y nodejs
-
-#install npm
-#sudo apt-get install -y npm
-sudo npm install -g npm
-
-#install bun
-#sudo snap install bun-js
-
-#<https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/>
-sudo apt-get install gnupg curl
-curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
-   sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
-	--dearmor
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
-sudo apt-get update -y
-sudo apt-get install -y mongodb-org
-sudo apt-get install -y mongodb-mongosh
-sudo systemctl enable mongod
-
-#json query / pretty-printer
+sudo apt-get install -y clang
+sudo apt-get install -y apt-transport-https xz-utils wget curl
 sudo apt-get install -y jq
 
-#typescript for global playing-around (project-specific preferred)
-sudo npm install -g typescript
-sudo npm install -g ts-node
+#ruby
+sudo apt-get install -y ruby
+
+#zig
+curl -fsSL \
+ https://ziglang.org/builds/zig-x86_64-linux-0.15.0-dev.1380+e98aeeb73.tar.xz \
+     > zig.tar.xz
+xzcat zig.tar.xz | tar -xf -
+sudo cp zig-x86_64-linux*/zig /usr/local/bin
+sudo cp -r zig-x86_64-linux*/lib /usr/local/lib/zig
+rm -rf zig*
 
 #editors
 sudo apt-get install -y xauth emacs gedit vim
@@ -72,22 +43,29 @@ sudo apt-get install -y xauth emacs gedit vim
 #sudo snap install atom --classic
 
 #visual studio code
-#<https://code.visualstudio.com/docs/setup/linux>
-sudo snap install --classic code
+#<https://code.visualstudio.com/docs/setup/linux#_install-vs-code-on-linux>
+# install apt-get repo and signing key
+echo "code code/add-microsoft-repo boolean true" | sudo debconf-set-selections
+sudo apt-get install -y wget gpg
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc \
+    | gpg --dearmor > microsoft.gpg
+sudo install -D -o root -g root -m 644 microsoft.gpg /usr/share/keyrings/microsoft.gpg
+rm -f microsoft.gpg
+echo 'Types: deb
+URIs: https://packages.microsoft.com/repos/code
+Suites: stable
+Components: main
+Architectures: amd64,arm64,armhf
+Signed-By: /usr/share/keyrings/microsoft.gpg'  \
+     | sudo tee /etc/apt/sources.list.d/vscode.sources >/dev/null
+sudo apt-get update -y; sudo apt-get install code
 
 #sublime: <https://www.sublimetext.com/docs/linux_repositories.html>
-wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | \
-    gpg --dearmor | \
-    sudo tee /etc/apt/trusted.gpg.d/sublimehq-archive.gpg > /dev/null
-echo "deb https://download.sublimetext.com/ apt/stable/" \
-    | sudo tee /etc/apt/sources.list.d/sublime-text.list
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg \
+    | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
+echo -e 'Types: deb\nURIs: https://download.sublimetext.com/\nSuites: apt/stable/\nSigned-By: /etc/apt/keyrings/sublimehq-pub.asc' \
+    | sudo tee /etc/apt/sources.list.d/sublime-text.sources
 sudo apt-get update -y && sudo apt-get install -y sublime-text
-
-#remote desktop vnc
-sudo apt-get install -y tightvncserver xtightvncviewer
-
-#install autocutsel for VNC copy text between client and server
-sudo apt-get install -y autocutsel
 
 #google-chrome <https://itsfoss.com/install-chrome-ubuntu/>
 sudo apt-get install -y xdg-utils
@@ -95,41 +73,67 @@ wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt-get install -y fonts-liberation
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-#ruby
-sudo apt-get install -y ruby
-
-#zig
-curl -fsSL \
- https://ziglang.org/builds/zig-linux-x86_64-0.14.0-dev.2627+6a21d18ad.tar.xz \
-     > zig.tar.xz
-xzcat zig.tar.xz | tar -xf -
-sudo cp zig-linux*/zig /usr/local/bin
-sudo cp -r zig-linux*/lib /usr/local/lib/zig
-rm -rf zig*
-
 #firefox
-#<https://askubuntu.com/questions/1516223/how-to-install-firefox-from-official-mozilla-repository-on-ubuntu-24-04>
-wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | gpg --dearmor | sudo tee /etc/apt/keyrings/packages.mozilla.org.gpg > /dev/null
-cat <<EOF | sudo tee /etc/apt/sources.list.d/mozilla.sources > /dev/null
-Types: deb
-URIs: https://packages.mozilla.org/apt
-Suites: mozilla
-Components: main
-Signed-By: /etc/apt/keyrings/packages.mozilla.org.gpg
-EOF
-cat <<EOF | sudo tee /etc/apt/preferences.d/mozilla > /dev/null
-Package: firefox*
+#<https://support.mozilla.org/en-US/kb/install-firefox-linux#w_install-firefox-deb-package-for-debian-based-distributions-recommended>
+# set up keyrings dir
+sudo install -d -m 0755 /etc/apt/keyrings
+# get signing key
+wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
+# update sources list
+echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt-get mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
+# prioritize Mozilla repo
+echo '
+Package: *
 Pin: origin packages.mozilla.org
-Pin-Priority: 1001
-EOF
-cat <<EOF | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox \
-		 > /dev/null
-Unattended-Upgrade::Origins-Pattern { "archive=mozilla"; };
-EOF
-sudo snap remove firefox
-sudo apt-get remove -y firefox
-sudo apt-get update -y
-sudo apt-get install -y firefox
+Pin-Priority: 1000
+' | sudo tee /etc/apt/preferences.d/mozilla
+# update package repos and install
+sudo apt-get update -y && sudo apt-get install -y firefox
 
-sudo timeshift --create --snapshot-device $TIMESHIFT_DEV
-sudo timeshift --list --snapshot-device $TIMESHIFT_DEV
+#remote desktop vnc
+sudo apt-get install -y tightvncserver xtightvncviewer
+
+#install autocutsel for VNC copy text between client and server
+sudo apt-get install -y autocutsel
+
+#<https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/>
+sudo apt-get install -y gnupg curl
+curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
+	--dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" \
+    | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+sudo apt-get update -y
+sudo apt-get install -y mongodb-org
+sudo apt-get install -y mongodb-mongosh
+sudo systemctl enable mongod
+
+## install rust within $HOME
+# curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+
+## install nodejs within $HOME
+## get nvm
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
+## install current version
+# nvm install 24
+
+## install typescript under node global ($HOME above)
+# npm install -g typescript
+# npm install -g ts-node
+
+##install bun
+#curl -fsSL https://bun.sh/install | bash
+
+
+
+
+#https://linuxconfig.org/ubuntu-22-04-system-backup-and-restore>
+#sudo apt-get install -y timeshift
+#TIMESHIFT_DEV=/dev/mapper/ubuntu--vg-ubuntu--lv
+#sudo timeshift --create --snapshot-device $TIMESHIFT_DEV
+#sudo timeshift --list --snapshot-device $TIMESHIFT_DEV
+
+
+#sudo timeshift --create --snapshot-device $TIMESHIFT_DEV
+#sudo timeshift --list --snapshot-device $TIMESHIFT_DEV
